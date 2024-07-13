871e9408947bfe0cb73edc7e51ba0f8fe77a1a19
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-13 03:15:28 +0000

    day 1 - 13/07/2024

---
+++ 01_Day/variables_and_data_types.js
@@ -0,0 +1,30 @@
+var number = 100;
+console.log(number);
+
+let string = "Dipendu";
+console.log(string);
+
+const boolean_value = Boolean(1);
+console.log(boolean_value);
+
+let myobj = {
+    name: "Dipendu Jana",
+    age : 21
+}
+console.log(myobj);
+console.log(typeof(myobj));
+
+let myarry = ["Spiderman","Batman","Superman"];
+console.log(myarry);
+console.log(typeof(myarry));
+
+string = "Dipenda Jana";  //reassigning string
+console.log(string);
+
+const mynameis = "Dipendu Jana"
+console.log(mynameis);             // const error
+
+mynameis = "Superman"
+console.log(mynameis);
+
+
