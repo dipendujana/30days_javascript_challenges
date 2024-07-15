e5f07f6d5ff87225dc51ced23037e1e79ae39bca
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-07-15 14:49:30 +0000

    operator

---
+++ 02_Days/operators.js
@@ -0,0 +1,76 @@
+// Activites 1: Arithmetic Operations
+
+// add to numbers
+let num1 = 20;
+let num2 = 10;
+let sum = num1+num2
+console.log(sum); //30
+
+// subtract two numbers
+let a = 20;
+let b = 10;
+let c = a-b;
+console.log(c); //10
+
+// multiply two numbers
+let x = 20;
+let y = 10;
+let z = x*y;
+console.log(z); //200
+
+// divide two numbers
+let o = 20;
+let p = 10;
+let q = o/p;
+console.log(q); //2
+
+// find remainder
+let j = 20;
+let k = 10;
+let l = j%k;
+console.log(l); //0
+
+// Activites 2: Assignment Operator
+
+let value = 20;
+value += 10;
+console.log(value);
+
+let value2 = 20;
+value2 -= 10;
+console.log(value2);
+
+// Activites 3: copmparison Operator
+console.log("// Activites 3: copmparison Operator");
+
+let n1 = 20 ;
+let n2 = 10 ;
+console.log(`n1 = ${n1} > n2 = ${n2}`,n1>n2); //true
+console.log(n1<n2);     //fales
+console.log(n1>=n2);    //true
+console.log(n1<=n2);    //fales
+console.log(n1==n2);    //fales
+console.log(n1===n2);   //fales
+
+// Activites 4: logical operators
+console.log("// Activites 4: logical operators");
+
+let number1 = 20 > 10 && 20<10;
+console.log(number1);       //false
+
+let number2 = 20 > 10 || 20>10;
+console.log(number2);       //true
+
+let number3 = 20 > 10 != 20>10;
+console.log(number3);       //false
+
+
+// Activites 4: ternary operators
+console.log("// Activites 4: ternary operators");
+
+let tar = 357;
+let val = ( tar % 2 == 0) ? 'Even Number' : 'Odd Number';
+console.log(val);           //Odd Number
+
+
+
